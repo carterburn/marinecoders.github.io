6b3407197237c92d2f3a998da5bc8c338e27c62e
carterburn <carter.burn3@gmail.com>
2020-09-17 08:51:39 -0400

    Update naval_letter.js

--- assets/js/naval_letter.js
+++ assets/js/naval_letter.js
@@ -1,1 +1,29 @@
 // this is a test
+function generate() {
+            const doc = new docx.Document();
+            
+            doc.addSection({
+                properties: {},
+                children: [
+                    new docx.Paragraph({
+                        children: [
+                            new docx.TextRun("Hello World"),
+                            new docx.TextRun({
+                                text: "Foo Bar",
+                                bold: true,
+                            }),
+                            new docx.TextRun({
+                                text: "\tGithub is the best",
+                                bold: true,
+                            }),
+                        ],
+                    }),
+                ],
+            });
+
+            docx.Packer.toBlob(doc).then(blob => {
+                console.log(blob);
+                saveAs(blob, "example.docx");
+                console.log("Document created successfully");
+            });
+}
